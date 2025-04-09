484897d7aa3f5f1dfba61ff0bd8eccba52597d41
lscblack <l.christian@alustudent.com>
2025-04-09 12:28:07 +0200

    added axios fetching for users from json placeholder

--- src/pages/feedBack.jsx
+++ src/pages/feedBack.jsx
@@ -1,10 +1,41 @@
+import { useEffect } from "react";
+import Footer from "../comps/Footer";
 import { Navbar } from "../comps/Navbar";
+import Service from "../comps/Srvice";
+import axios from "axios"
+import { useState } from "react";
+export function Feedback() {
+    const [users,usersData] = useState([])
+    useEffect(()=>{
+        axios.get("https://jsonplaceholder.typicode.com/users/")
+        .then((res)=>{
+            usersData(res.data)
+        })
+        .catch((eror)=>{
+            console.log(eror)
+        })
+    },[])
+    return (
+        <>
+            <Navbar />
 
-export function Feedback(){
-    return(
-    <>
-    <Navbar/> <h1> console</h1>
+            {/** Main Body Contents */}
+            {users.map((data)=>(
+                <>
+                <div className="flex gap-3 p-3">
+                    <div></div>
+                    <div>
+                        <p>{data.name}</p>
+                        <p>{data.username}</p>
+                    </div>
+                </div>
+                </>
+            ))}
+            <Service />
+            {/** End of Main Body Contents */}
 
-       </>  
+            <Footer />
+
+        </>
     )
 }
